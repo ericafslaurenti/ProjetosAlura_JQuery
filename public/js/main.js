46867af3c7f2aa95a77074ee4e34708e85930b33
Erica Laurenti <ericafs.laurenti@gmail.com>
2021-04-22 20:14:45 -0300

    Aula 2
    Exercício praticado em aula

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,18 @@ var tamanhoFrase = $("#tamanho-frase");//pega meu span - seleciona o meu nº da 
 
 tamanhoFrase.text(numPalavras);//troca o valor do span usando o .text passando o nº de palavras\\
 
+var campo = $(".campo-digitacao");
+campo.on("input", function() {//qdo alguém estiver colocando dados no meu campo, essa fç é executada - o evento de input é um marcador de caracteres\\
+    var conteudo = campo.val();//.val acessa o valor dos inputs do usuário\\
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1; // (/\S+/)expressão regular q busca por qlq espaço vazio. o -1 deixa o jogo + preciso\\
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+
 //$atalho pra fç Jquery//
 //.text() - fç usada p/ obter o texto da minha frase\\
 //.text(numPalavras) qdo passada sem argumento ela pega, qd passado com, ela coloca\\
